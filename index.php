ec267e8ec78c634996c6f274e90db23826c8c263
Korchevskiy <korhyn@gmail.com>
2021-10-28 12:37:57 +0300

    edit index PHP and create SQL request file

--- index.php
+++ index.php
@@ -4,12 +4,3 @@ include_once 'functions.php';
 include_once 'User.php';
 $users = new \Civenty\User();
 
-$users->addUser($user);
-//$number=[
-//    'countryCode' =>380,
-//    'operatorCode' =>50,
-//    'number'=> 7654321
-//];
-//var_dump($users->topUpPhone($number, 1));
-//$users->deleteUser(1934);
-//380-50-7654321
